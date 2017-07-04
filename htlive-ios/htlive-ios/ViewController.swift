976f9b05302bca83588ee459b3a853b179d8e10e
VIBES <VIBHAS111@GMAIL.COM>
2017-07-04 12:29:16 +0530

    First commit blank project

---
+++ htlive-ios/htlive-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  htlive-ios
+//
+//  Created by Vibes on 7/4/17.
+//  Copyright © 2017 PZRT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
