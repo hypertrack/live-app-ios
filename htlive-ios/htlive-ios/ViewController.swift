e3d103f59de5a0a58ba9998659e025b8a5236ce3
Ravi Jain <ravijain.bits@gmail.com>
2017-07-18 18:54:59 -0700

    removed hardcoded user id

--- htlive-ios/htlive-ios/ViewController.swift
+++ htlive-ios/htlive-ios/ViewController.swift
@@ -52,7 +52,6 @@ class ViewController: UIViewController {
         
         placeLineTable.register(UINib(nibName: "placeCell", bundle: nil), forCellReuseIdentifier: "placeCell")
         
-        HyperTrack.setUserId("2966354f-9ecc-44f8-a28b-3a804d5eb93c")
         HyperTrack.getPlaceline { (placeLine, error) in
             guard let fetchedPlaceLine = placeLine else { return }
             if let segments = fetchedPlaceLine.segments {
